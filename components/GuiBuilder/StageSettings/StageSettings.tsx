e4964495cd907ef1b8fca1c1a9c1d8fc48bd0fb0
AnsonH <ansonheung2000@gmail.com>
2022-11-05 20:23:56 +0800

    feat(gui): stage settings empty and unsupported states

--- components/GuiBuilder/StageSettings/StageSettings.tsx
+++ components/GuiBuilder/StageSettings/StageSettings.tsx
@@ -2,6 +2,8 @@ import supportedStages, { SupportedStage } from "@constants/Config/supportedStag
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Tooltip } from "@mantine/core";
 import { useStoreState } from "@state/GuiBuilder/Hooks";
+import Image from "next/image";
+import UnsupportedStage from "./UnsupportedStage";
 
 /**
  * Settings panel for an individual stage in the pipeline.
@@ -13,23 +15,26 @@ function StageSettings() {
     return <NoStageSelected />;
   }
 
-  const supportedStage: SupportedStage = supportedStages[selectedStage.name];
-  const StageSettings = supportedStage.stageSettings;
+  const stageName = selectedStage.name;
+  const supportedStage: SupportedStage | undefined = supportedStages[stageName];
+  const StageSettings = supportedStage?.stageSettings ?? UnsupportedStage;
 
   return (
     <div className="w-full">
       <div className="p-3 flex justify-between border-b border-gray-300">
         <div className="flex self-center">
-          <h2 className="text-xl font-semibold">{supportedStage.label}</h2>
-          <Tooltip
-            label={supportedStage.description}
-            transition="fade"
-            position="right"
-            withArrow
-            className="ml-2 p-1 flex self-center text-gray-500"
-          >
-            <FontAwesomeIcon icon={["far", "circle-info"]} />
-          </Tooltip>
+          <h2 className="text-xl font-semibold">{supportedStage?.label ?? stageName}</h2>
+          {supportedStage && (
+            <Tooltip
+              label={supportedStage.description}
+              transition="fade"
+              position="right"
+              withArrow
+              className="ml-2 p-1 flex self-center text-gray-500"
+            >
+              <FontAwesomeIcon icon={["far", "circle-info"]} />
+            </Tooltip>
+          )}
         </div>
       </div>
       <StageSettings />
@@ -39,10 +44,19 @@ function StageSettings() {
 
 function NoStageSelected() {
   return (
-    <div className="h-full flex flex-col items-center justify-center">
-      <div className="space-y-3 text-center">
-        <p className="text-lg font-medium">Select a stage in the pipeline editor to configure its settings.</p>
-        <p>(TODO: Refine the UI of this panel)</p>
+    <div className="h-full px-5 flex flex-col gap-6 items-center justify-center">
+      <div className="flex items-center gap-3 text-lg text-blue-500">
+        <FontAwesomeIcon icon={["far", "circle-question"]} />
+        <p className="font-medium">To configure a stage, click the stage block in the pipeline editor.</p>
+      </div>
+      <div className="overflow-hidden">
+        <Image
+          src="/assets/gui_editor_select_stage.svg"
+          alt="pipeline editor demo"
+          width={300}
+          height={150}
+          className="rounded-xl"
+        />
       </div>
     </div>
   );
---
+++ components/GuiBuilder/StageSettings/UnsupportedStage.tsx
@@ -0,0 +1,13 @@
+/**
+ * For stages that are not yet supported by the GUI Assignment Builder.
+ */
+function UnsupportedStage() {
+  return (
+    <div className="p-3">
+      <p className="font-medium">This stage is not yet supported by the GUI Assignment Builder.</p>
+      <p className="mt-4">(TODO: Unfinished UI)</p>
+    </div>
+  );
+}
+
+export default UnsupportedStage;
