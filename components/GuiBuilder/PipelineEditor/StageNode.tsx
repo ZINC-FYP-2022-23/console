61d82a5930fc526cd8934c88eb8c8085b52c82ad
AnsonH <ansonheung2000@gmail.com>
2023-02-20 13:34:50 +0800

    feat(gui-pipeline): validate stage node connection

--- components/GuiBuilder/PipelineEditor/StageNode.tsx
+++ components/GuiBuilder/PipelineEditor/StageNode.tsx
@@ -1,9 +1,12 @@
 import { useStoreActions, useStoreState } from "@/store/GuiBuilder";
-import { StageNodeData } from "@/types/GuiBuilder";
+import { StageDependencyGraph, StageNodeData } from "@/types/GuiBuilder";
+import { transposeGraph } from "@/utils/GuiBuilder";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { clsx, Tooltip } from "@mantine/core";
+import { analyzeGraphFast, Graph } from "graph-cycles";
+import cloneDeep from "lodash/cloneDeep";
 import { DragEventHandler, useState } from "react";
-import { Handle, NodeProps, Position } from "reactflow";
+import { Connection, Handle, NodeProps, Position } from "reactflow";
 
 /**
  * Fired when a stage block is dragged over this stage node.
@@ -16,15 +19,52 @@ const onDragOver: DragEventHandler<HTMLDivElement> = (event) => {
 };
 
 /**
+ * Checks whether the connection between two nodes is valid.
+ *
+ * The connection is invalid if adding the connection:
+ *  - Makes a node having an out-degree > 1 in the stage execution order graph.
+ *  - Creates a cycle in the stage dependency graph.
+ */
+export const validateConnection = (connection: Connection, stageDeps: StageDependencyGraph) => {
+  const { source, target } = connection;
+  if (source === null || target === null || source === target) {
+    return false;
+  }
+
+  const newStageDeps = cloneDeep(stageDeps);
+  newStageDeps[target] = [...newStageDeps[target], source];
+
+  // Branched DAGs are currently not supported.
+  const newStageExecutionOrderGraph = transposeGraph(newStageDeps);
+  if (Object.values(newStageExecutionOrderGraph).some((children) => children.length > 1)) {
+    return false;
+  }
+
+  /**
+   * An array representation of the stage dependency graph.
+   * @example
+   * const newStageDeps = { A: ["B"], B: ["C"], C: [] }  // C <- B <- A
+   * const newStageDepsInArray = [["A", ["B"]], ["B", ["C"]], ["C", []]]
+   */
+  const newStageDepsInArray = Object.entries(newStageDeps).reduce<Graph>((graph, [id, children]) => {
+    const output = [...graph];
+    output.push([id, children]);
+    return output;
+  }, []);
+  const { cyclic } = analyzeGraphFast(newStageDepsInArray);
+  return cyclic.length === 0;
+};
+
+/**
  * A custom React Flow node that represents a stage in the grading pipeline.
  */
 function StageNode({ id, data, selected }: NodeProps<StageNodeData>) {
   const [isDragOver, setIsDragOver] = useState(false);
+
   const stageData = useStoreState((state) => state.config.editingConfig.stageData);
+  const stageDeps = useStoreState((state) => state.config.editingConfig.stageDeps);
   const dragging = useStoreState((state) => state.pipelineEditor.dragging);
-  const setModal = useStoreActions((actions) => actions.layout.setModal);
   const setShouldFocusLabelInput = useStoreActions((actions) => actions.pipelineEditor.setShouldFocusLabelInput);
-  const duplicateStage = useStoreActions((action) => action.pipelineEditor.duplicateStage);
 
   const stageLabel = stageData[id]?.label ?? "";
 
@@ -50,9 +90,18 @@ function StageNode({ id, data, selected }: NodeProps<StageNodeData>) {
           })(selected, isDragOver),
         )}
       >
-        {/* TODO(Anson): Validate handle connection with `isValidConnection` */}
-        <Handle className="!p-[5px] !border-2 !bg-cse-600 !-right-[7px]" type="source" position={Position.Right} />
-        <Handle className="!p-[5px] !border-2 !bg-cse-600 !-left-[7px]" type="target" position={Position.Left} />
+        <Handle
+          type="source"
+          position={Position.Right}
+          isValidConnection={(connection) => validateConnection(connection, stageDeps)}
+          className="!p-[5px] !border-2 !bg-cse-600 !-right-[7px]"
+        />
+        <Handle
+          type="target"
+          position={Position.Left}
+          isValidConnection={(connection) => validateConnection(connection, stageDeps)}
+          className="!p-[5px] !border-2 !bg-cse-600 !-left-[7px]"
+        />
         {/* When dragging a new stage on top of existing stage, adding "pointer-events-none" avoids firing `dragleave` event
          * in the parent when mouse is over the below div. */}
         <div className={clsx("flex flex-col items-center gap-1", dragging && "pointer-events-none")}>
@@ -68,26 +117,38 @@ function StageNode({ id, data, selected }: NodeProps<StageNodeData>) {
           )}
         </div>
       </div>
-      {selected && (
-        <div className="absolute left-[50%] -bottom-11 translate-x-[-50%] flex gap-5">
-          <Tooltip label="Duplicate stage" position="bottom" openDelay={500}>
-            <button
-              onClick={() => duplicateStage(id)}
-              className="w-8 h-8 flex justify-center items-center text-white bg-green-600 drop-shadow rounded-full hover:bg-green-800 transition"
-            >
-              <FontAwesomeIcon icon={["far", "copy"]} />
-            </button>
-          </Tooltip>
-          <Tooltip label="Delete stage" position="bottom" openDelay={500}>
-            <button
-              onClick={() => setModal({ path: "deleteStage", value: true })}
-              className="w-8 h-8 flex justify-center items-center text-white bg-red-500 drop-shadow rounded-full hover:bg-red-700 transition"
-            >
-              <FontAwesomeIcon icon={["far", "trash-can"]} />
-            </button>
-          </Tooltip>
-        </div>
-      )}
+      {selected && <StageNodeActionButtons id={id} />}
+    </div>
+  );
+}
+
+interface StageNodeActionButtonsProps {
+  id: string;
+}
+
+/** Action buttons to show if the stage node is selected. */
+function StageNodeActionButtons({ id }: StageNodeActionButtonsProps) {
+  const setModal = useStoreActions((actions) => actions.layout.setModal);
+  const duplicateStage = useStoreActions((action) => action.pipelineEditor.duplicateStage);
+
+  return (
+    <div className="absolute left-[50%] -bottom-11 translate-x-[-50%] flex gap-5">
+      <Tooltip label="Duplicate stage" position="bottom" openDelay={500}>
+        <button
+          onClick={() => duplicateStage(id)}
+          className="w-8 h-8 flex justify-center items-center text-white bg-green-600 drop-shadow rounded-full hover:bg-green-800 transition"
+        >
+          <FontAwesomeIcon icon={["far", "copy"]} />
+        </button>
+      </Tooltip>
+      <Tooltip label="Delete stage" position="bottom" openDelay={500}>
+        <button
+          onClick={() => setModal({ path: "deleteStage", value: true })}
+          className="w-8 h-8 flex justify-center items-center text-white bg-red-500 drop-shadow rounded-full hover:bg-red-700 transition"
+        >
+          <FontAwesomeIcon icon={["far", "trash-can"]} />
+        </button>
+      </Tooltip>
     </div>
   );
 }
