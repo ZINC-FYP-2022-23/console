3eb71b45f297119b60347e1c501ff2598c1bf457
AnsonH <ansonheung2000@gmail.com>
2023-04-03 16:49:10 +0800

    refactor(gui-diagnostics): tidy up types and remove dummy code

--- types/GuiBuilder/Diagnostics.ts
+++ types/GuiBuilder/Diagnostics.ts
@@ -10,12 +10,22 @@ import { Config, Settings } from "@/types/GuiBuilder";
  * describes any problems found. This list will be processed and converted to {@link ConfigDiagnostics}.
  */
 export interface DiagnosticRaw {
+  /** An ID to identify the type of the diagnostic (e.g. `"MISSING_FIELD_ERROR"`). */
   type: string;
+  /**
+   * Human readable message of the diagnostic.
+   *
+   * It may not be suitable to directly display this message in the UI. This is because it may contain the
+   * key name of the erroneous YAML field, which may confuse the users if shown to them.
+   */
   message: string;
+  /** Diagnostic severity. */
   severity: "WARNING" | "ERROR";
+  /** Location of the diagnostic if any. */
   location?: {
     /** Key of the stage (e.g. `"compile:main"`, `"diffWithSkeleton"`). */
     stage?: string;
+    /** Test case ID. Relevant to certain stages such as `StdioTest`. */
     testCaseId?: number;
   };
 }
@@ -35,29 +45,12 @@ export interface ConfigDiagnostics {
 /**
  * A diagnostic that describes a problem in the config YAML.
  */
-export interface Diagnostic {
+export interface Diagnostic extends DiagnosticRaw {
   /**
    * A helper field to indicate whether this diagnostic is resolved.
    *
-   * A diagnostic can be resolved if the user has fixed the problem or has chosen to ignore it. The
-   * diagnostic will be hidden from the UI if it's resolved.
+   * A diagnostic can be resolved if the user has fixed the problem or has chosen to ignore it. Once it's
+   * marked resolved, the UI should hide the diagnostic.
    */
   resolved: boolean;
-
-  /** An ID to identify the type of the diagnostic (e.g. `"MISSING_FIELD_ERROR"`). */
-  type: string;
-  /**
-   * Human readable message. It may not be suitable to directly display this message in the UI since it
-   * may contain the key name of the erroneous YAML field.
-   */
-  message: string;
-  /** Diagnostic severity. */
-  severity: "WARNING" | "ERROR";
-  /** Location of the diagnostic if any. */
-  location?: {
-    /** Key of the stage (e.g. `"compile:main"`, `"diffWithSkeleton"`). */
-    stage?: string;
-    /** Test case ID. Relevant to certain stages such as `StdioTest`. */
-    testCaseId?: number;
-  };
 }
